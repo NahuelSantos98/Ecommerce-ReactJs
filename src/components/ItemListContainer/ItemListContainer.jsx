d582c87837acdcb64065f82233484574dcd2acbf
Nahuel Santos <santosnahuel98@gmail.com>
2024-01-31 12:06:16 -0300

    modificaciones en el firebaseConfig e implementacion del mismo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { getProducts, getProductsByCategory } from '../../../asyncMock'
+import {db} from '../../services/firebaseConfig'
+import {collection, query, where, getDocs} from 'firebase/firestore'
 import ItemList from '../ItemList/ItemList'
 import './ItemListContainer.css'
 import { useParams } from 'react-router-dom'
+
 
 const ItemListContainer = ({ greetings }) => {
     const styleGreetings = {color: "black", fontSize: 50, marginTop:40};
     const [products, setProducts] = useState([])
     const { category } = useParams()
 
-
 useEffect(() => {
+
+    const productsRef = category ? query(collection(db, 'products'), where('category', '===', category))
+    : collection(db, 'products')
+
+    getDocs(productsRef).then(snapshot=>{
+        const data = snapshot.docs.map(doc=> doc.data())
+        console.log(data)
+    })
+
+    
     if (category) {
         getProductsByCategory(category)
         .then((res) => {
