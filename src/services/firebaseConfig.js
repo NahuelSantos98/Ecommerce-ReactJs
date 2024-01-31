eaca96a05aed695c11dd59cd72cbead1a53e1c9f
Nahuel Santos <santosnahuel98@gmail.com>
2024-01-30 22:15:19 -0300

    cambios en el .env

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -6,21 +6,16 @@ import {getFirestore} from  'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1H0Im-jReTGgnLDFQ7YqsrPUvdIxF9QM",
-  authDomain: "ecommerce-react-e152c.firebaseapp.com",
-  projectId: "ecommerce-react-e152c",
-  storageBucket: "ecommerce-react-e152c.appspot.com",
-  messagingSenderId: "549969788299",
-  appId: "1:549969788299:web:2a39a130eaa43024d7c38e"
+  apiKey: import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+  appId:import.meta.env. VITE_APP_APPID,
 };
 
 
-//apiKey: import.meta.env.VITE_APP_APIKEY,
-//authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
-//projectId: import.meta.env.VITE_APP_PROJECTID,
-//storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
-//messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
-//appId:import.meta.env. VITE_APP_APPID,
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
